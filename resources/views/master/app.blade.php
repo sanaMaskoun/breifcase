cea7abf1111acd770031c1c1370d7915692d0d2f
sana mohammed <sana.maskoun2001@gmail.com>
2024-03-24 23:06:46 +0300

    test public

--- resources/views/master/app.blade.php
+++ resources/views/master/app.blade.php
@@ -12,25 +12,25 @@
 
     <!-- GOOGLE FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
-    <link href="{{ asset('assets/plugins/material/css/materialdesignicons.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/plugins/simplebar/simplebar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('public/assets/plugins/material/css/materialdesignicons.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('public/assets/plugins/simplebar/simplebar.css') }}" rel="stylesheet" />
 
     <!-- PLUGINS CSS STYLE -->
-    <link href="{{ asset('assets/plugins/nprogress/nprogress.css') }}" rel="stylesheet" />
+    <link href="{{ asset('public/assets/plugins/nprogress/nprogress.css') }}" rel="stylesheet" />
 
 
 
 
-    <link href="{{ asset('assets/plugins/DataTables/DataTables-1.10.18/css/jquery.dataTables.min.css') }}"
+    <link href="{{ asset('public/assets/plugins/DataTables/DataTables-1.10.18/css/jquery.dataTables.min.css') }}"
         rel="stylesheet" />
 
 
 
-    <link href="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css') }}" rel="stylesheet" />
+    <link href="{{ asset('public/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css') }}" rel="stylesheet" />
 
 
 
-    <link href="{{ asset('assets/plugins/daterangepicker/daterangepicker.css') }}" rel="stylesheet" />
+    <link href="{{ asset('public/assets/plugins/daterangepicker/daterangepicker.css') }}" rel="stylesheet" />
 
 
 
@@ -38,18 +38,18 @@
 
 
 
-    <link href="{{ asset('assets/plugins/toaster/toastr.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('public/assets/plugins/toaster/toastr.min.css') }}" rel="stylesheet" />
 
 
     <!-- MONO CSS -->
-    <link id="main-css-href" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+    <link id="main-css-href" rel="stylesheet" href="{{ asset('public/assets/css/style.css') }}" />
 
 
 
 
     <!-- FAVICON -->
     <link href="images/favicon.png" rel="shortcut icon" />
-    <script src="{{ asset('assets/plugins/nprogress/nprogress.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/nprogress/nprogress.js') }}"></script>
 </head>
 
 
@@ -74,29 +74,29 @@
 
 
 
-    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/simplebar/simplebar.min.js') }}"></script>
     <script src="https://unpkg.com/hotkeys-js/dist/hotkeys.min.js"></script>
 
 
 
-    <script src="{{ asset('assets/plugins/apexcharts/apexcharts.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/apexcharts/apexcharts.js') }}"></script>
 
 
 
-    <script src="{{ asset('assets/plugins/DataTables/DataTables-1.10.18/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/DataTables/DataTables-1.10.18/js/jquery.dataTables.min.js') }}"></script>
 
 
 
-    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-world-mill.js') }}"></script>
-    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-us-aea.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/jvectormap/jquery-jvectormap-world-mill.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/jvectormap/jquery-jvectormap-us-aea.js') }}"></script>
 
 
 
-    <script src="{{ asset('assets/plugins/daterangepicker/moment.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/daterangepicker/moment.min.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
     <script>
         jQuery(document).ready(function() {
             jQuery('input[name="dateRange"]').daterangepicker({
@@ -121,14 +121,14 @@
 
 
 
-    <script src="{{ asset('assets/plugins/toaster/toastr.min.js') }}"></script>
+    <script src="{{ asset('public/assets/plugins/toaster/toastr.min.js') }}"></script>
 
 
 
-    <script src="{{ asset('assets/js/mono.js') }}"></script>
-    <script src="{{ asset('assets/js/chart.js') }}"></script>
-    <script src="{{ asset('assets/js/map.js') }}"></script>
-    <script src="{{ asset('assets/js/custom.js') }}"></script>
+    <script src="{{ asset('public/assets/js/mono.js') }}"></script>
+    <script src="{{ asset('public/assets/js/chart.js') }}"></script>
+    <script src="{{ asset('public/assets/js/map.js') }}"></script>
+    <script src="{{ asset('public/assets/js/custom.js') }}"></script>
 
 </body>
 
